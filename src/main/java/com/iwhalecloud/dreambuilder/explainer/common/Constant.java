4b42c4df09fbeaab2586fd3a3a19a101416ef2f4
bfen <358780026@qq.com>
2019-10-03 17:13:29 +0800

    去除无用的常量

--- src/main/java/com/iwhalecloud/dreambuilder/explainer/common/Constant.java
+++ src/main/java/com/iwhalecloud/dreambuilder/explainer/common/Constant.java
@@ -7,21 +7,5 @@ package com.iwhalecloud.dreambuilder.explainer.common;
  * @date 2019/9/30
  */
 public class Constant {
-    /**
-     * datasource.url配置项
-     */
-    public static final String DATASOURCE_URL = "datasource.url";
-    /**
-     * datasource.username配置项
-     */
-    public static final String DATASOURCE_USERNAME = "datasource.username";
-    /**
-     * datasource.password配置项
-     */
-    public static final String DATASOURCE_PASSWORD = "datasource.password";
-    /**
-     * datasource.driverClass配置项
-     */
-    public static final String DATASOURCE_DRIVER = "datasource.driverClass";
 
 }
