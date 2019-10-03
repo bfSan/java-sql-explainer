80837135cc8495511427f1f96d47d96779c245e0
bfen <358780026@qq.com>
2019-10-03 16:55:25 +0800

    完善ExplainContext

--- src/main/java/com/iwhalecloud/dreambuilder/explainer/model/ExplainContext.java
+++ src/main/java/com/iwhalecloud/dreambuilder/explainer/model/ExplainContext.java
@@ -7,5 +7,41 @@ package com.iwhalecloud.dreambuilder.explainer.model;
 public class ExplainContext {
     private ExplainBean explainBean;
     private String methodName;
-    private String mapperClass;
+    private String mapperClassName;
+
+    public ExplainContext() {
+    }
+
+    public ExplainBean getExplainBean() {
+        return explainBean;
+    }
+
+    public void setExplainBean(ExplainBean explainBean) {
+        this.explainBean = explainBean;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public String getMapperClassName() {
+        return mapperClassName;
+    }
+
+    public void setMapperClassName(String mapperClassName) {
+        this.mapperClassName = mapperClassName;
+    }
+
+    @Override
+    public String toString() {
+        return "ExplainContext{" +
+                "explainBean=" + explainBean +
+                ", methodName='" + methodName + '\'' +
+                ", mapperClassName='" + mapperClassName + '\'' +
+                '}';
+    }
 }
